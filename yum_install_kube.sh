b3f33bdd7ee67ffe25ce980cacbf29bb3f239e8b
Stephen Sennett <ssennett@ssennett.net>
2021-12-20 20:54:50 +1100

    Update to Kube1.13.5

--- yum_install_kube.sh
+++ yum_install_kube.sh
@@ -1,1 +1,1 @@
-yum install -y kubelet-1.13.3-0 kubeadm-1.13.3-0 kubectl-1.13.3-0 kubernetes-cni-0.6.0 --disableexcludes=kubernetes
+yum install -y kubelet-1.13.5-0 kubeadm-1.13.5-0 kubectl-1.13.5-0 kubernetes-cni-0.7.5 --disableexcludes=kubernetes
